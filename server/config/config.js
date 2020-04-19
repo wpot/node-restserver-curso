7221ada72afdd745e4fb00cb999460806230a2fa
omar wpot <omarwpot100@gmail.com>
2020-04-18 19:30:50 -0500

    variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.NODE_ENV.MONGO_URI;
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
